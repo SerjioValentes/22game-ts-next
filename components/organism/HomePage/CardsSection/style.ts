94aed0e2d47df2c93d033d0c46acf6b679e55c4d
SerjioValentes <vasilkamalov@gmail.com>
2023-11-04 20:43:42 +0300

    resolve usedispatch error build v -1

--- components/organism/HomePage/CardsSection/style.ts
+++ components/organism/HomePage/CardsSection/style.ts
@@ -1,12 +1,12 @@
-'use client';
+// 'use client';
 
-import theme from '@/helpers/ThemeProvider';
-import styled from '@emotion/styled';
+// import theme from '@/helpers/ThemeProvider';
+// import styled from '@emotion/styled';
 
-const FirstSectionWrapper = styled('div')({
-  // justifyitems: 'center',
-  // margin: 'auto',
-  background: theme.palette.primary.main,
-});
+// const FirstSectionWrapper = styled('div')({
+//   // justifyitems: 'center',
+//   // margin: 'auto',
+//   background: theme.palette.primary.main,
+// });
 
-export default FirstSectionWrapper;
+// export default FirstSectionWrapper;
